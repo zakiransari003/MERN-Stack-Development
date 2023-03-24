9f3fb5aee151c41165daeeb2cd8172b58ed6b3a4
ZAKIR ANSARI <zakiransari068@gmail.com>
2023-03-24 22:47:41 +0500

    new files of modules import export added

---
+++ node/callFunction.js
@@ -0,0 +1,11 @@
+// const displayMessage = () => {
+//     console.log("Exporting Module in another file ");
+//   }
+  
+//   module.exports = displayMessage;
+
+const  displayMessage =()=> {
+    console.log("Exporting Module in another file ");
+  }
+
+  export default displayMessage;
---
+++ node/index.js
@@ -0,0 +1,4 @@
+// const  displayMessage = require('./callFunction');
+import displayMessage from "./callFunction.js"
+
+displayMessage();
